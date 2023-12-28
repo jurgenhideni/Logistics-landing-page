0c6ca73d266c7fdaa95e0fb37ef985003dfd612d
Jurgen Hideni <jurgenhideni@gmail.com>
2023-12-28 16:07:57 +0100

    changed design of website

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,39 +1,123 @@
 import React from "react";
-import Image from "next/image";
-import image1 from "../assets/image1.png";
-import Link from "next/link";
+import ilustration from "../assets/ilustration.png"
+import { BiSolidShip } from "react-icons/bi";
+import { BiSolidPlaneAlt } from "react-icons/bi";
+import { FaWarehouse } from "react-icons/fa";
+import { FaTruckLoading } from "react-icons/fa";
 
+import Link from "next/link";
+import Image from "next/image";
 
 const MainSection = () => {
   return (
-    <div className="max-sm:block flex items-center justify-between lg:gap-10  ">
-      <Image
-        className="lg:max-h-[950px] lg:w-auto max-sm:min-w-[800px]"
-        src={image1}
-        alt="image"
-      />
+    <>
+   <div className=" lg:ml-16 text-white lg:min-h-[90vh] lg:flex lg:items-center lg:justify justify-between">
+    <div className="lg:max-w-[700px] flex flex-col max-sm:text-center max-sm:mx-16  gap-2">
+      <h1 className="lg:font-bold lg:text-[3.5rem] 
+      max-sm:text-[4rem] 
+      mt-10 max-sm:font-bold">YOUR TRUSTED PARTNER IN LOGISTICS EXELLENCE!</h1>
+     <p >We specialize in providing seamless logistics solutions tailored to meet your unique needs.
+      Our Company is committed to delivering efficiency, reliability, and cost-effectiveness to optimize your supply chain.
+      Explore our comprehensive range of services, including transportation, warehousing, and supply chain management.
+     </p>
+     
+     <Image src={ilustration}  alt="photo" className="lg:w-[600px] lg:h-[600px] mx-auto md:hidden" />
+     <Link href="/about" className="text-center text-[1.2rem]
+     lg:rounded-lg lg:hover:bg-white lg:hover:text-blue-500  lg:font-bold lg:px-10 py-4 bg-blue-500 max-sm:text-[3rem] 
+     max-sm:font-bold">FIND OUT MORE...
+     </Link>
+    
+     </div>
+    <Image src={ilustration}  alt="photo" className="lg:w-[600px] lg:h-[600px] mr-10 max-sm:hidden" />
+   </div>
 
-      <div className="grid gap-10 grid-cols-1 max-sm:min-w-[600px]  max-sm:ml-[5rem]">
-        <h1 className="font-bold text-rose-700 text-[5rem]  max-sm:text-[4rem] ">
-          A new Perspective of Delivering Goods
-        </h1>
-        <h2 className=" my-[50px] font-semibold text-rose-700 text-[1.5rem] text-left ">
-          Efficient Packing for Seamless Shipping. Trust Our Logistics Experts
-          to Handle Your Packaging Needs and Ensure Safe Delivery Every Time.
-        </h2>
-        <div className="max-md:grid max-md:justify-center">
-          <Link
-            href="/about"
-            className=" w-[150px] max-sm:w-[260px] text-center py-3 text-white px-3 
-          font-bold text-[1.5rem] rounded-md max-sm:text-[2.5rem] 
-          transition ease-in-out bg-rose-700 hover:text-rose-700 hover:bg-white shadow-md"
-          >
-            More Info...
-          </Link>
-         
-        </div>
-      </div>
-    </div>
+   <div className="lg:px-20 bg-white flex justify-between max-sm:hidden">
+    <div>
+      <h2 className="font-bold text-[1.5rem] bg-slate-200 w-[200px] text-center"><span className="text-blue-500 font-extrabold " >|</span>  WHAT WE DO </h2>
+      <h2 className="mt-10 text-[2.5rem] font-bold text-blue-500">Safe and Reliable <br /> Cargo Solutions</h2>
+  </div>
+   
+<div className="grid grid-cols-2 gap-10 max-w-[1150px] lg:py-10">
+
+  <div id="1" className="flex gap-5 lg:shadow-lg lg:py-10 lg:px-5">
+  <BiSolidPlaneAlt
+ className="lg:my-1 lg:text-yellow-500 lg:text-[5rem]" />
+  <div> 
+    <h2 className="font-bold text-blue-500 text-[2rem] ">Air Fright Services</h2>
+    <p>Our logistics company's air freight service is 
+      characterized by a commitment to timely deliveries,
+       providing a dependable solution for your urgent shipping needs.</p>
+  </div>
+ </div>
+  <div id="2" className="flex gap-5 lg:shadow-lg lg:py-10 lg:px-5">
+  <BiSolidShip className="lg:my-1 lg:text-yellow-500 lg:text-[5rem]" />
+  <div> 
+    <h2 className="font-bold text-blue-500 text-[2rem]">Sea Transport Services</h2>
+    <p>Our logistics company offers a reliable sea transport service, ensuring the efficient and secure delivery 
+      of your cargo across international waters.</p>
+  </div>
+ </div>
+  <div id="3" className="flex gap-5 lg:shadow-lg lg:py-10 lg:px-5">
+  <FaWarehouse className="lg:my-1 lg:text-yellow-500 lg:text-[5rem]" />
+  <div> 
+    <h2 className="font-bold text-blue-500 text-[2rem]">Warehousing Services</h2>
+    <p>Our logistics company offers a reliable sea transport service, ensuring the efficient and secure delivery 
+      of your cargo across international waters.</p>
+  </div>
+ </div>
+  <div id="4" className="flex gap-5 lg:shadow-lg lg:py-10 lg:px-5">
+  <FaTruckLoading  className="lg:my-1 lg:text-yellow-500 lg:text-[5rem]" />
+  <div> 
+    <h2 className="font-bold text-blue-500 text-[2rem]">Local Shipping Services</h2>
+    <p>Our logistics company offers a reliable sea transport service, ensuring the efficient and secure delivery 
+      of your cargo across international waters.</p>
+  </div>
+ </div>
+
+</div>
+
+   </div>
+
+   <div id="Smartphone" className="md:hidden bg-white h-full mt-10" >
+<h1 className="text-[3.5rem] font font-extrabold text-blue-500 text-center py-10">WHAT WE DO </h1>
+   <div id="1" className="flex gap-5 shadow-lg py-10 px-5 mx-10">
+  <BiSolidPlaneAlt
+ className="my-1 text-yellow-500 text-[5rem]" />
+  <div> 
+    <h2 className="font-bold text-blue-500 text-[3rem] ">Air Fright Services</h2>
+    <p className="text-[1.5rem]">Our logistics company's air freight service is 
+      characterized by a commitment to timely deliveries,
+       providing a dependable solution for your urgent shipping needs.</p>
+  </div>
+ </div>
+ <div id="2" className="flex gap-5 shadow-lg py-10 px-5 mx-10">
+  <BiSolidShip
+ className="my-1 text-yellow-500 text-[5rem]" />
+  <div> 
+    <h2 className="font-bold text-blue-500 text-[3rem] ">Sea Transport Services</h2>
+    <p className="text-[1.5rem]">Our logistics company offers a reliable sea transport service, ensuring the efficient and secure delivery 
+      of your cargo across international waters.</p>
+  </div>
+ </div>
+ <div id="1" className="flex gap-5 shadow-lg py-10 px-5 mx-10">
+  <FaTruckLoading
+ className="my-1 text-yellow-500 text-[5rem]" />
+  <div> 
+    <h2 className="font-bold text-blue-500 text-[3rem] ">Local Shipping Services</h2>
+    <p className="text-[1.5rem]">Our logistics company offers a reliable sea transport service, ensuring the efficient and secure delivery of your cargo across international waters.</p>
+  </div>
+ </div>
+ <div id="1" className="flex gap-5 shadow-lg py-10 px-5 mx-10">
+  <FaWarehouse
+ className="my-1 text-yellow-500 text-[5rem]" />
+  <div> 
+    <h2 className="font-bold text-blue-500 text-[3rem] ">Warehousing Services</h2>
+    <p className="text-[1.5rem]">Our logistics company offers a reliable sea transport service, ensuring the efficient and secure delivery of your cargo across international waters.</p>
+  </div>
+ </div>
+     
+   </div>
+   </>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,32 @@
 import React from "react";
 import Link from "next/link";
+import { IoHome } from "react-icons/io5";
+import { FaInfoCircle } from "react-icons/fa";
+import { FaRegPaperPlane } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between mx-16 max-sm:mx-0 gap-10 max-sm:gap-[100px] ">
-      <div>
-        <h1 className="font-bold text-[4.5rem] text-center text-rose-600 mx-5 ">
+    <div className="lg:bg-blue-500 flex justify-between max-sm:mx-0 gap-10 max-sm:gap-[100px] lg:px-10 sticky">
+      <div className="max-sm:hidden ">
+        <h1 className="  font-bold text-[3rem] text-center text-white">
           HLS
         </h1>
-        <p className="font-bold text-lg max-sm:hidden">
-          Hideni Logistics Services™
+        <p className=" lg:pb-5 font-bold text-white text-lg max-sm:hidden">
+          Logistics Services™
         </p>
       </div>
-      <div className="uppercase flex gap-[90px] items-center font-bold text-rose-700 text-xl max-sm:text-[1.8rem] ">
+      <div id="Desktop" className=" max-sm:hidden uppercase flex gap-[90px] items-center font-bold text-white text-xl max-sm:text-[1.8rem] ">
         <Link
           className="lg:hover:scale-125 transition 
-          ease-in-out lg:hover:text-white lg:hover:bg-rose-700 
+          ease-in-out  
           lg:px-2 lg:py-1 rounded-lg"
           href="/"
         >
-          Home
+          Home 
         </Link>
         <Link
           className="lg:hover:scale-125 transition ease-in-out 
-          lg:hover:text-white lg:hover:bg-rose-700 lg:px-2
+            lg:px-2
           lg:py-1 rounded-lg max-sm:min-w-[120px]"
           href="/about"
         >
@@ -31,13 +34,20 @@ const Navbar = () => {
         </Link>
         <Link
           className="lg:hover:scale-125 transition ease-in-out 
-          lg:hover:text-white lg:hover:bg-rose-700 lg:px-2
+            lg:px-2
           lg:py-1 rounded-lg max-sm:mx-5 "
           href="/contacts"
         >
           Contacts
         </Link>
       </div>
+
+<div id="Smartphone" className="md:hidden flex text-[6rem]  text-blue-500 justify-around  w-full bg-slate-800 shadow-lg ">
+<Link href="/" className="active:bg-white p-10 "><IoHome /></Link>
+<Link href="/about" className="active:bg-white p-10 "><FaInfoCircle /></Link>
+<Link href="/contacts" className="active:bg-white p-10 "><FaRegPaperPlane /></Link>
+</div >
+
     </div>
   );
 };
